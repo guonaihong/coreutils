8d20a66062358ffbe22b7c1e2745b84c1499cd48
guonaihong <guonaihong@qq.com>
2018-11-26 22:25:25 +0800

    调度ok

--- tee.go
+++ tee.go
@@ -7,11 +7,51 @@ import (
 	"github.com/guonaihong/log"
 	"io"
 	"os"
+	"os/signal"
+	"syscall"
 )
+
+func ignore() {
+	signal.Ignore(syscall.SIGABRT)
+	signal.Ignore(syscall.SIGALRM)
+	signal.Ignore(syscall.SIGBUS)
+	signal.Ignore(syscall.SIGCHLD)
+	signal.Ignore(syscall.SIGCLD)
+	signal.Ignore(syscall.SIGCONT)
+	signal.Ignore(syscall.SIGFPE)
+	signal.Ignore(syscall.SIGHUP)
+	signal.Ignore(syscall.SIGILL)
+	signal.Ignore(syscall.SIGINT)
+	signal.Ignore(syscall.SIGIO)
+	signal.Ignore(syscall.SIGIOT)
+	signal.Ignore(syscall.SIGKILL)
+	signal.Ignore(syscall.SIGPIPE)
+	signal.Ignore(syscall.SIGPOLL)
+	signal.Ignore(syscall.SIGPROF)
+	signal.Ignore(syscall.SIGPWR)
+	signal.Ignore(syscall.SIGQUIT)
+	signal.Ignore(syscall.SIGSEGV)
+	signal.Ignore(syscall.SIGSTKFLT)
+	signal.Ignore(syscall.SIGSTOP)
+	signal.Ignore(syscall.SIGSYS)
+	signal.Ignore(syscall.SIGTERM)
+	signal.Ignore(syscall.SIGTRAP)
+	//signal.Ignore(syscall.SIGTSTP) //ctrl-z
+	signal.Ignore(syscall.SIGTTIN)
+	signal.Ignore(syscall.SIGTTOU)
+	signal.Ignore(syscall.SIGUNUSED)
+	signal.Ignore(syscall.SIGURG)
+	signal.Ignore(syscall.SIGUSR1)
+	signal.Ignore(syscall.SIGUSR2)
+	signal.Ignore(syscall.SIGVTALRM)
+	signal.Ignore(syscall.SIGWINCH)
+	signal.Ignore(syscall.SIGXCPU)
+	signal.Ignore(syscall.SIGXFSZ)
+}
 
 func main() {
 	append := flag.Bool("a, append", false, "append to the given FILEs, do not overwrite")
-	//ignoreInterrupts := flag.Bool("i, ignore-interrupts", false, "ignore interrupt signals")//todo
+	ignoreInterrupts := flag.Bool("i, ignore-interrupts", false, "ignore interrupt signals") //todo
 	gzip := flag.Bool("g, gzip", false, "compressed archived log files")
 	maxSize := flag.String("s, max-size", "0", "current file maximum write size")
 	maxArchive := flag.Int("A, max-archive", 0, "How many archive files are saved")
@@ -20,11 +60,20 @@ func main() {
 
 	var fileArch *log.File
 	var buffer [4096]byte
-
+	var fileName string
 	var w io.Writer
 
+	if *ignoreInterrupts {
+		ignore()
+	}
+
 	args := flag.Args()
-	fileName := args[0]
+	if len(args) == 0 {
+		w = os.Stdout
+		goto end
+	}
+
+	fileName = args[0]
 	if *maxSize != "" {
 		size, err := file.ParseSize(*maxSize)
 		if err != nil {
@@ -55,11 +104,15 @@ func main() {
 		w = file
 	}
 
+end:
 	for {
 		n, err := os.Stdin.Read(buffer[:])
 		if err != nil {
 			break
 		}
 		w.Write(buffer[:n])
+		if len(args) != 0 {
+			os.Stdout.Write(buffer[:n])
+		}
 	}
 }
