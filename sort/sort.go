9b41670ecdde037b2e1845476c70557f3183cbb0
guonaihong <guonaihong@qq.com>
2019-01-29 23:04:15 +0800

    sort: add -b -M -R -z option

--- sort/sort.go
+++ sort/sort.go
@@ -25,6 +25,64 @@ func isoctal(b byte) bool {
 	return false
 }
 
+type Month int8
+
+const (
+	Unknown Month = iota
+	January
+	February
+	March
+	April
+	May
+	June
+	July
+	August
+	September
+	October
+	November
+	December
+)
+
+const emptyStr = "\t\n\v\f\r \x85\xA0"
+
+var lineDelim byte = '\n'
+
+func parseMonth(s []byte) Month {
+	m := string(bytes.ToUpper(bytes.TrimLeft(s, emptyStr)))
+	if len(m) >= 3 {
+		m = m[:3]
+	}
+
+	switch m {
+	case "JAN":
+		return January
+	case "FEB":
+		return February
+	case "MAR":
+		return March
+	case "APR":
+		return April
+	case "MAY":
+		return May
+	case "JUN":
+		return June
+	case "JUL":
+		return July
+	case "AUG":
+		return August
+	case "SEP":
+		return September
+	case "OCT":
+		return October
+	case "NOV":
+		return November
+	case "DEC":
+		return December
+	}
+
+	return Unknown
+}
+
 func isOctalStr(s string, max int) (i int, haveOctal bool) {
 	for i = 0; i < len(s); i++ {
 		if i >= max {
@@ -63,15 +121,15 @@ func (sl *sortLine) isNumber() bool {
 }
 
 func main() {
-	flag.String("b, ignore-leading-blanks", "", "ignore leading blanks")
-	flag.String("d, dictionary-order", "", "consider only blanks and alphanumeric characters")
+	ignoreLeadingBlanks := flag.Bool("b, ignore-leading-blanks", false, "ignore leading blanks")
+	dictionaryOrder := flag.Bool("d, dictionary-order", false, "consider only blanks and alphanumeric characters")
 	ignoreCase := flag.Bool("f, ignore-case", false, "fold lower case to upper case characters")
 	flag.String("g, general-numeric-sort", "", "compare according to general numerical value")
 	flag.String("i, ignore-nonprinting", "", "consider only printable characters")
-	flag.String("M, month-sort", "", "compare (unknown) < 'JAN' < ... < 'DEC'")
-	flag.String("h, human-numeric-sort", "", "compare human readable numbers (e.g., 2K 1G)")
+	monthSort := flag.Bool("M, month-sort", false, "compare (unknown) < 'JAN' < ... < 'DEC'")
+	humanNumericSort := flag.Bool("h, human-numeric-sort", false, "compare human readable numbers (e.g., 2K 1G)")
 	numericSort := flag.Bool("n, numeric-sort", false, "compare according to string numerical value")
-	flag.String("R, random-sort", "", "shuffle, but group identical keys.  See shuf(1)")
+	randomSort := flag.Bool("R, random-sort", false, "shuffle, but group identical keys.  See shuf(1)")
 	flag.String("random-source", "", "get random bytes from FILE")
 	reverse := flag.Bool("r, reverse", false, "reverse the result of comparisons")
 	flag.String("sort", "", "sort according to WORD: general-numeric -g, human-numeric -h, month -M, numeric -n, random -R, version -V")
@@ -87,14 +145,18 @@ func main() {
 	output := flag.String("o, output", "", "write result to FILE instead of standard output")
 	flag.String("s, stable", "", "stabilize sort by disabling last-resort comparison")
 	flag.String("S, buffer-size", "", "use SIZE for main memory buffer")
-	flag.String("t, field-separator=SEP", "", "use SEP instead of non-blank to blank transition")
+	flag.String("t, field-separator", "", "use SEP instead of non-blank to blank transition")
 	flag.String("T, temporary-directory=DIR", "", "use DIR for temporaries, not $TMPDIR or /tmp; multiple options specify multiple directories")
 	flag.String("parallel", "", "change the number of sorts run concurrently to N")
 	unique := flag.Bool("u, unique", false, "with -c, check for strict ordering; without -c, output only the first of an equal run")
-	flag.String("z, zero-terminated", "", "line delimiter is NUL, not newline")
+	zeroTerminated := flag.Bool("z, zero-terminated", false, "line delimiter is NUL, not newline")
 
 	flag.Parse()
 	args := flag.Args()
+
+	if *zeroTerminated {
+		lineDelim = byte(0)
+	}
 
 	defaultCmp := func(allLine []sortLine, i, j int) bool {
 		cmp := func(allLine []sortLine, i, j int) bool {
@@ -103,6 +165,25 @@ func main() {
 			if *ignoreCase {
 				aLine = bytes.ToUpper(aLine)
 				bLine = bytes.ToUpper(bLine)
+			}
+
+			if *ignoreLeadingBlanks {
+				aLine = bytes.TrimLeft(aLine, emptyStr)
+				bLine = bytes.TrimLeft(bLine, emptyStr)
+			}
+
+			if *humanNumericSort {
+			}
+
+			if *dictionaryOrder {
+			}
+
+			diff := 0
+			if *monthSort {
+				diff = int(parseMonth(aLine)) - int(parseMonth(bLine))
+				if diff != 0 {
+					return diff < 0
+				}
 			}
 
 			if *numericSort {
@@ -132,16 +213,20 @@ func main() {
 		br := bufio.NewReader(r)
 
 		var allLine []sortLine
-
+		lineMap := map[int]struct{}{}
 		uniqueMap := map[string]struct{}{}
 
-		for {
-			l, _, e := br.ReadLine()
+		for count := 0; ; count++ {
+			l, e := br.ReadBytes(lineDelim)
 			if e == io.EOF {
 				break
 			}
 
 			l = append(l, '\n')
+
+			if *randomSort {
+				lineMap[count] = struct{}{}
+			}
 
 			if *unique {
 				key := string(l)
@@ -155,6 +240,14 @@ func main() {
 		}
 
 		sort.Slice(allLine, func(i, j int) bool { return defaultCmp(allLine, i, j) })
+
+		if len(lineMap) > 0 {
+			for k, _ := range lineMap {
+				w.Write(allLine[k].line)
+			}
+
+			return
+		}
 
 		for _, v := range allLine {
 			w.Write(v.line)
