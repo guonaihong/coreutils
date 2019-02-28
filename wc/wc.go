0e1262f4d20182059c97eb4057c033fe6803f194
guonaihong <guonaihong@qq.com>
2019-02-28 23:02:18 +0800

    wc:update code

--- wc/wc.go
+++ wc/wc.go
@@ -1,21 +1,56 @@
 package wc
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"github.com/guonaihong/coreutils/utils"
 	"github.com/guonaihong/flag"
+	"os"
 )
 
 type wcCmd struct {
 	bytesCounts     *bool
 	characterCounts *bool
 	lines           *bool
-	files0From      *bool
+	files0From      *string
 	maxLineLength   *bool
 	words           *bool
 }
 
-func Main(args []string) {
-	command := flag.NewFlagSet(args[0], flag.ContinueOnError)
+func (w *wcCmd) main(fd *os.File) {
+	br := bufio.NewReader(fd)
+
+	totalBytes, totalLine, maxLineLength := 0, 0, 0
+	totalWords := 0
+
+	for {
+		l, e := br.ReadBytes('\n')
+		if e != nil && len(l) == 0 {
+			break
+		}
+
+		ls := bytes.Split(l, []byte{' '})
+		for _, v := range ls {
+			if len(bytes.TrimSpace(v)) == 0 {
+				continue
+			}
+			totalWords++
+		}
+
+		totalBytes += len(l)
+		totalLine++
+		if len(l) > maxLineLength {
+			maxLineLength = len(l)
+		}
+	}
+
+	fmt.Printf("%4d  %4d  %4d  %s\n", totalLine, totalWords, totalBytes, fd.Name())
+
+}
+
+func Main(argv []string) {
+	command := flag.NewFlagSet(argv[0], flag.ContinueOnError)
 
 	wc := wcCmd{}
 
@@ -25,7 +60,7 @@ func Main(args []string) {
 	wc.characterCounts = command.Opt("m, chars", "print the character counts").
 		Flags(flag.PosixShort).NewBool(false)
 
-	wc.lines = command.Opt("print the newline counts").
+	wc.lines = command.Opt("l, lines", "print the newline counts").
 		Flags(flag.PosixShort).NewBool(false)
 
 	wc.files0From = command.Opt("files0-from", "read input from the files specified by\n"+
@@ -39,4 +74,15 @@ func Main(args []string) {
 	wc.words = command.Opt("w, words", "print the word counts").
 		Flags(flag.PosixShort).NewBool(false)
 
+	command.Parse(argv[1:])
+
+	args := command.Args()
+	for _, v := range args {
+		fd, err := utils.OpenInputFd(v)
+		if err != nil {
+			utils.Die("wc: %s\n", err)
+		}
+		wc.main(fd)
+		utils.CloseInputFd(fd)
+	}
 }
