4692c7b10a7e86fbabee5db85e901cfd26735401
guonaihong <guonaihong@qq.com>
2019-03-18 19:57:54 +0800

    tac: add New

--- tac/tac.go
+++ tac/tac.go
@@ -1,7 +1,37 @@
 package tac
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/guonaihong/flag"
+)
 
-func main() {
-	fmt.Println("vim-go")
+type Tac struct {
+	Before    *bool
+	Regex     *string
+	Separator *string
+}
+
+func New(argv []string) (*Tac, []string) {
+	t := Tac{}
+	command := flag.NewFlagSet(argv[0], flag.ExitOnError)
+	t.Before = command.Opt("b, before", "attach the separator before instead of after").
+		Flags(flag.PosixShort).
+		NewBool(false)
+	t.Regex = command.Opt("r, regex", "interpret the separator as a regular expression").
+		Flags(flag.PosixShort).
+		NewString("")
+	t.Separator = command.Opt("s, separator", "use STRING as the separator instead of newline").
+		Flags(flag.PosixShort).
+		NewString("")
+
+	command.Parse(argv[1:])
+	args := command.Args()
+
+	return &t, args
+}
+
+func (t *Tac) Tac(rs io.ReadSeeker, w io.Writer) {
+}
+
+func Main(argv []string) {
 }
---
+++ tac/tac_test.go
@@ -0,0 +1,14 @@
+package tac
+
+import (
+	"testing"
+)
+
+func TestBefore(t *testing.T) {
+}
+
+func TestRegex(t *testing.T) {
+}
+
+func Testseparator(t *testing.T) {
+}
