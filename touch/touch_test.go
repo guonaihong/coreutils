9a2a150fb0e7a706b49e0d5a21bd487293eb4c67
guonaihong <guonaihong@qq.com>
2019-04-08 22:28:52 +0800

    touch: update test file, todo build

--- touch/touch_test.go
+++ touch/touch_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/guonaihong/coreutils/utils"
 	"os"
 	"testing"
+	"time"
 )
 
 const fileName1 = "touch-test-file1"
@@ -13,6 +14,25 @@ func testParseTime(t *testing.T) {
 }
 
 func TestParseTime(t *testing.T) {
+}
+
+func testParseDate(date string, needDate time.Time, t *testing.T) {
+	touch := Touch{}
+	t1, err := touch.parseDate(date)
+	if err != nil {
+		t.Errorf("touch -d fail:%s\n", err)
+	}
+
+	if !t1.Equal(needDate) {
+		t.Errorf("needDate(%v), date(%v)\n", needDate, t1)
+	}
+}
+
+func TestParseDate(t *testing.T) {
+	now := time.Now()
+	testParseDate("1 May 2005 10:22", time.Date(2005, 5, 1, 10, 22, 0, 0, time.Local))
+	testParseDate("14 May", time.Date(now.Year(), 5, 14, 0, 0, 0, 0, time.Local))
+	testParseDate("14:24", time.Date(now.Year(), now.Month(), now.Day(), 14, 24, 0, 0, time.Local))
 }
 
 //touch -c file
