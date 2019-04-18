ef1c59bc324dfeaf9cb6be74830e8c2f1f47bdf3
guonaihong <guonaihong@qq.com>
2019-04-18 22:37:47 +0800

    chown: -c -v -h option ok

--- chown/chown.go
+++ chown/chown.go
@@ -29,6 +29,18 @@ type Chown struct {
 	P              *bool
 }
 
+type User struct {
+	Uid int
+	Gid int
+	W   io.Writer
+}
+
+func (u *User) Init(w io.Writer) {
+	u.Uid = -1
+	u.Gid = -1
+	u.W = w
+}
+
 func New(argv []string) (*Chown, []string) {
 	c := &Chown{}
 
@@ -91,9 +103,29 @@ func New(argv []string) (*Chown, []string) {
 	return c, command.Args()
 }
 
+type chownUser user.User
+
+func (u *chownUser) String() string {
+	if u == nil {
+		return ""
+	}
+
+	return u.Username
+}
+
+type chownGroup user.Group
+
+func (g *chownGroup) String() string {
+	if g == nil {
+		return ""
+	}
+	return g.Name
+}
+
 type groupUser struct {
-	group *user.Group
-	user  *user.User
+	group *chownGroup
+	user  *chownUser
+	name  string
 }
 
 func getGroupUser(name string) (*groupUser, error) {
@@ -101,12 +133,10 @@ func getGroupUser(name string) (*groupUser, error) {
 	var names []string
 
 	if name == ":" || name == "." {
-		return &groupUser{}, nil
+		return &groupUser{name: name}, nil
 	}
 
-	if strings.Index(name, ":") != -1 {
-		names = strings.Split(name, ":")
-	}
+	names = strings.Split(name, ":")
 
 	if strings.Index(name, ".") != -1 {
 		names = strings.Split(name, ".")
@@ -136,7 +166,7 @@ func getGroupUser(name string) (*groupUser, error) {
 		}
 	}
 
-	return &groupUser{user: u, group: g}, nil
+	return &groupUser{user: (*chownUser)(u), group: (*chownGroup)(g), name: name}, nil
 }
 
 func checkArgs(args []string) error {
@@ -182,6 +212,24 @@ func (c *Chown) IsVerbose() bool {
 	return c.Verbose != nil && *c.Verbose
 }
 
+func noChanages(gu *groupUser, fileUser *user.User, fileGroup *user.Group) bool {
+	return (gu.group == nil || gu.group.Gid == fileUser.Gid) &&
+		(gu.user == nil || gu.user.Uid == fileUser.Uid)
+}
+
+func findGroup(name string) bool {
+	return !(strings.Index(name, ":") == -1 && strings.Index(name, ".") == -1)
+}
+
+func genToName(gu *groupUser) (to string) {
+	to = fmt.Sprintf("%s:%s", gu.user, gu.group)
+	if !findGroup(gu.name) || len(gu.name) > 0 && gu.name[0] == ':' {
+		to = gu.name
+		fmt.Printf("to = %s\n", to)
+	}
+	return
+}
+
 func (c *Chown) printVerbse(fileName string, gu *groupUser, w io.Writer) error {
 	var st unix.Stat_t
 
@@ -211,31 +259,44 @@ func (c *Chown) printVerbse(fileName string, gu *groupUser, w io.Writer) error {
 	}
 
 	if c.IsChanges() {
-		if gu.group.Gid == fileUser.Gid && gu.user.Uid == fileUser.Uid {
+		if !noChanages(gu, fileUser, fileGroup) {
 			goto next
 		}
 		return nil
 	}
 
-	if gu.group.Gid == fileUser.Gid && gu.user.Uid == fileUser.Uid {
+	if noChanages(gu, fileUser, fileGroup) {
 
 		fmt.Fprintf(w,
-			"ownership of '%s' retained as %s:%s\n",
-			fileName, fileUser.Username, fileGroup.Name)
+			"ownership of '%s' retained as %s\n",
+			fileName, genToName(gu))
 		return nil
 	}
 
 next:
+
+	from := fmt.Sprintf("%s:%s", fileUser.Username, fileGroup.Name)
+	if !findGroup(gu.name) {
+		from = fileUser.Username
+	}
+
 	fmt.Fprintf(w,
-		"changed ownership of '%s' from %s:%s to %s:%s\n",
-		fileName, fileUser.Username, fileGroup.Name,
-		gu.user.Username, gu.group.Name)
+		"changed ownership of '%s' from %s to %s\n",
+		fileName, from,
+		genToName(gu))
 
 	return nil
 }
 
-func (c *Chown) Chown(name string, fileName string, w io.Writer) error {
+// user: or user.
+func isEndSplitter(name string) bool {
+	last := len(name) - 1
+	return len(name) > 0 && (name[last] == ':' || name[last] == '.')
+}
 
+func (c *Chown) Chown(name string, fileName string, u *User) error {
+
+	w := u.W
 	groupUser, err := getGroupUser(name)
 	if err != nil {
 		return err
@@ -250,21 +311,13 @@ func (c *Chown) Chown(name string, fileName string, w io.Writer) error {
 		gid, _ = strconv.Atoi(groupUser.group.Gid)
 	}
 
-	if gid == -1 && uid != -1 {
-		groupUser.group, err = user.LookupGroupId(groupUser.user.Uid)
+	if gid == -1 && uid != -1 && isEndSplitter(name) {
+		g, err := user.LookupGroupId(groupUser.user.Uid)
 		if err != nil {
 			return err
 		}
+		groupUser.group = (*chownGroup)(g)
 		gid = uid
-	}
-
-	if uid == -1 && gid != -1 {
-		//groupUser.user = &user.User{}
-		groupUser.user, err = user.LookupId(groupUser.group.Gid)
-		if err != nil {
-			return err
-		}
-		uid = gid
 	}
 
 	if c.IsChanges() || c.IsVerbose() {
@@ -274,7 +327,16 @@ func (c *Chown) Chown(name string, fileName string, w io.Writer) error {
 		}
 	}
 
-	err = os.Chown(fileName, uid, gid)
+	u.Uid = uid
+	u.Gid = gid
+
+	chown := os.Chown
+
+	if c.NoDereference != nil && *c.NoDereference {
+		chown = os.Lchown
+	}
+
+	err = chown(fileName, uid, gid)
 	if err != nil {
 		return formatError(fileName, err)
 	}
@@ -291,8 +353,10 @@ func Main(argv []string) {
 		utils.Die("%s\n", err)
 	}
 
+	u := User{}
+	u.Init(os.Stdout)
 	for _, a := range args[1:] {
-		err := c.Chown(args[0], a, os.Stdout)
+		err := c.Chown(args[0], a, &u)
 		if err != nil {
 			fmt.Printf("%s\n", err)
 		}
--- chown/chown_test.go
+++ chown/chown_test.go
@@ -8,6 +8,13 @@ import (
 	"testing"
 )
 
+const (
+	binUid  = 2
+	binGid  = 2
+	rootUid = 0
+	rootGid = 0
+)
+
 func testGetGroupUserError(name string, needErr string, t *testing.T) {
 	_, err := getGroupUser(name)
 	if err.Error() != needErr {
@@ -30,14 +37,16 @@ func testChown(name string, fileName string, needErr string, rootRun bool, t *te
 		return
 	}
 
+	user := User{}
+	user.Init(os.Stdout)
 	c := Chown{}
-	err = c.Chown(name, fileName, os.Stdout)
+	err = c.Chown(name, fileName, &user)
 	if err.Error() != needErr {
 		t.Errorf("need error(%s), actual error(%s)\n", needErr, err.Error())
 	}
 }
 
-func testChownVerbose(name string, out string, t *testing.T) {
+func testChownVerbose(name string, out string, uid int, gid int, t *testing.T) {
 	u, err := user.Current()
 	if err != nil {
 		t.Errorf("%s\n", err)
@@ -53,24 +62,79 @@ func testChownVerbose(name string, out string, t *testing.T) {
 
 	var w bytes.Buffer
 
+	user := User{}
+	user.Init(&w)
 	os.Chown("test.dat", 2, 2)
 
 	c.Verbose = utils.Bool(true)
-	err = c.Chown(name, "test.dat", &w)
+	err = c.Chown(name, "test.dat", &user)
 	if w.String() != out || w.String() == "" {
-		t.Errorf("need(%s), actual(%s), rv(%v)\n", out, w.String(), err)
+		t.Errorf("need(%s), actual(%s), rv(%v), name(%s)\n",
+			out, w.String(), err, name)
 	}
 
+	if user.Uid != uid || user.Gid != gid {
+		t.Errorf("name (%s) need uid(%d) gid(%d), actual uid(%d) gid(%d)\n",
+			name, uid, gid, user.Uid, user.Gid)
+	}
 }
 
 // need root user to run
 func TestChownVerbose(t *testing.T) {
-	testChownVerbose(":", "ownership of 'test.dat' retained\n", t)
-	testChownVerbose("bin:", "ownership of 'test.dat' retained as bin:bin\n", t)
-	testChownVerbose(":bin", "ownership of 'test.dat' retained as bin:bin\n", t)
-	testChownVerbose("root:", "changed ownership of 'test.dat' from bin:bin to root:root\n", t)
-	testChownVerbose(":root", "changed ownership of 'test.dat' from bin:bin to :root\n", t)
-	testChownVerbose("root", "changed ownership of 'test.dat' from bin to root\n", t)
+	testChownVerbose(":", "ownership of 'test.dat' retained\n", -1, -1, t)
+	testChownVerbose("bin", "ownership of 'test.dat' retained as bin\n", binUid, -1, t)
+	testChownVerbose("bin:", "ownership of 'test.dat' retained as bin:bin\n", binUid, binGid, t)
+	testChownVerbose(":bin", "ownership of 'test.dat' retained as :bin\n", -1, binUid, t)
+
+	testChownVerbose("root:", "changed ownership of 'test.dat' from bin:bin to root:root\n", rootUid, rootGid, t)
+	testChownVerbose(":root", "changed ownership of 'test.dat' from bin:bin to :root\n", -1, rootGid, t)
+
+	testChownVerbose("root", "changed ownership of 'test.dat' from bin to root\n", rootUid, -1, t)
+}
+
+func testChownChanges(name string, out string, uid int, gid int, t *testing.T) {
+	u, err := user.Current()
+	if err != nil {
+		t.Errorf("%s\n", err)
+		return
+	}
+
+	if u.Username != "root" {
+		t.Errorf("need root user\n")
+		return
+	}
+
+	c := Chown{}
+
+	var w bytes.Buffer
+
+	user := User{}
+	user.Init(&w)
+	os.Chown("test.dat", 2, 2)
+
+	c.Changes = utils.Bool(true)
+	err = c.Chown(name, "test.dat", &user)
+	if w.String() != out {
+		t.Errorf("need(%s), actual(%s), rv(%v), name(%s)\n",
+			out, w.String(), err, name)
+	}
+
+	if user.Uid != uid || user.Gid != gid {
+		t.Errorf("name (%s) need uid(%d) gid(%d), actual uid(%d) gid(%d)\n",
+			name, uid, gid, user.Uid, user.Gid)
+	}
+}
+
+func TestChownChanges(t *testing.T) {
+	testChownChanges(":", "", -1, -1, t)
+	testChownChanges("bin", "", binUid, -1, t)
+	testChownChanges("bin:", "", binUid, binGid, t)
+	testChownChanges(":bin", "", -1, binUid, t)
+
+	testChownChanges("root:", "changed ownership of 'test.dat' from bin:bin to root:root\n", rootUid, rootGid, t)
+	testChownChanges(":root", "changed ownership of 'test.dat' from bin:bin to :root\n", -1, rootGid, t)
+
+	testChownChanges("root", "changed ownership of 'test.dat' from bin to root\n", rootUid, -1, t)
 }
 
 func TestChown(t *testing.T) {
