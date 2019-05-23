63c9cd1375d5f9986410c404bcb2830b959362dd
guonaihong <guonaihong@qq.com>
2019-05-23 22:27:42 +0800

    pwd: update

--- pwd/pwd.go
+++ pwd/pwd.go
@@ -7,16 +7,25 @@ import (
 )
 
 type Pwd struct {
-	L *bool
-	P *bool
+	Logical  bool
+	Physical bool
 }
 
 func New(argv []string) (*Pwd, []string) {
 	p := Pwd{}
 
 	command := flag.NewFlagSet(argv[0], flag.ExitOnError)
-	command.Opt()
+
+	command.Opt("L, logical", "print the value of $PWD if it names the current working\n"+
+		"directory").Flags(flag.PosixShort).Var(&p.Logical)
+
+	command.Opt("P, physical", "print the physical directory, without any symbolic links").
+		Flags(flag.PosixShort).Var(&p.Physical)
+
+	command.Parse(argv[1:])
+	return &p, command.Args()
 }
 
 func Main(argv []string) {
+	p, args := New(argv)
 }
